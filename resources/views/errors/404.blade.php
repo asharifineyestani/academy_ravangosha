4f217e988e23eeb1bc9d201265294d6995cfc05a
Ali Sharifineyestani <asharifineyestani@gmail.com>
2024-03-19 19:35:02 -0700

    minor modify

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,18 +1,23 @@
-@extends('layouts.tv.master')
-@section('content')
-    <div class="page-404 section--full-bg" data-bg="img/bg.jpg">
-        <div class="container">
-            <div class="row">
-                <div class="col-12">
-                    <div class="page-404__wrap">
-                        <div class="page-404__content">
-                            <h1 class="page-404__title">404</h1>
-                            <p class="page-404__text">این صفحه در دست ساخت است.</p>
-                            <a href="/" class="page-404__btn">رفتن به برگ نخست</a>
-                        </div>
+@extends('layouts.edu.master')
+@section('main')
+    <main>
+        <section class="pt-5">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 text-center">
+                        <!-- Image -->
+                        <img src="assets/images/element/error404-01.svg" class="h-200px h-md-400px mb-4" alt="">
+                        <!-- Title -->
+                        <h1 class="display-1 text-danger mb-0">404</h1>
+                        <!-- Subtitle -->
+                        <h2>این صفحه در دست ساخت است</h2>
+                        <!-- info -->
+                        <p class="mb-4">کارشناسان ما در حال کار بر یوی این برگه هستند</p>
+                        <!-- Button -->
+                        <a href="/" class="btn btn-primary mb-0">برگشت به برگ نخست</a>
                     </div>
                 </div>
             </div>
-        </div>
-    </div>
+        </section>
+    </main>
 @endsection
